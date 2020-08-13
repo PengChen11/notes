d8782816f6f5baedc1c9063e1ce4b6ca0b5bb604
Peng Chen <pchen0816@outlook.com>
2020-08-12 19:38:19 -0700

    phase 3

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 'use strict';
 
-// node modules
-// const minimist = require('minimist');
+
+const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost:27017/notesy', {useNewUrlParser: true, useUnifiedTopology: true});
+
 const Input = require('./lib/input');
 const Notes = require('./lib/notes');
 
 // new instance of constructor function from input.js file
 const userInput = new Input();
+const newNote = new Notes();
 
-if (userInput.valid()) new Notes(userInput);
-else console.log('ERROR! Please use -a or --add as method to add notes, and make sure the notes is not empty');
+if (userInput.valid()) {
+  console.log(userInput);
+  newNote.execute(userInput);
+  // .then(mongoose.disconnect)
+  // .catch(err=> console.error(err));
+}
+else console.log('ERROR! Please use proper method to add, delete, notes, and make sure the notes is not empty');
